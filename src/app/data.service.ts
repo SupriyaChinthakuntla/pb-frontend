c3bc61e1e07d3e598b9eb4cba9a5f8ace7657420
Priya Reddy <saipriyareddy94@gmail.com>
2020-12-19 03:03:45 -0500

    Added https

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -36,7 +36,7 @@ getData(username): Observable<any> {
     const token = localStorage.getItem('accessToken');
     const body = JSON.stringify(username);
     const headers = {'content-type': 'application/json','Authorization' : `Bearer ${token}`};
-    this.DataObservable = this.http.get('http://personalbudgetbackend.herokuapp.com/budget',{ headers: headers,params:{userid : username }}).pipe(shareReplay());
+    this.DataObservable = this.http.get('https://personalbudgetbackend.herokuapp.com/budget',{ headers: headers,params:{userid : username }}).pipe(shareReplay());
     return this.DataObservable;
 
 }
@@ -45,21 +45,21 @@ postBudget(data:BudgetSchema) {
   const token = localStorage.getItem('accessToken');
   const headers = {'content-type': 'application/json', 'Authorization' : `Bearer ${token}`};
   const body = JSON.stringify(data);
-  return this.http.post('http://personalbudgetbackend.herokuapp.com/budget',body,{'headers':headers});
+  return this.http.post('https://personalbudgetbackend.herokuapp.com/budget',body,{'headers':headers});
 }
 
 
 signUp(data:UserSchema) {
   const headers = {'content-type': 'application/json'};
   const body = JSON.stringify(data);
-  return this.http.post('http://personalbudgetbackend.herokuapp.com/users',body,{'headers':headers});
+  return this.http.post('https://personalbudgetbackend.herokuapp.com/users',body,{'headers':headers});
 }
 
 
 userLogin(data : UserSchema) {
   const headers = {'content-type': 'application/json'};
   const body = JSON.stringify(data);
-  return this.http.post('http://personalbudgetbackend.herokuapp.com/auth',body,{'headers':headers}).subscribe((res:any) => {
+  return this.http.post('https://personalbudgetbackend.herokuapp.com/auth',body,{'headers':headers}).subscribe((res:any) => {
     console.log( "dddd" + res);
     this.userRecord['username'] = data.username;
     this.userRecord['password'] = data.password;
